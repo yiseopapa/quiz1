bb4f5b9cb971e8029dca13eb60e0caeb03656b25
yiseopapa <dksemffl90@gmail.com>
2026-04-12 23:06:56 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,15 @@
-const CACHE = 'quiz-v1';
+const CACHE = 'quiz-v2';
 const FILES = ['./index.html'];
-
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(c) { return c.addAll(FILES); })
-  );
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
-    })
-  );
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) { return r || fetch(e.request); })
-  );
+self.addEventListener('fetch',function(e){
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request);}));
 });
